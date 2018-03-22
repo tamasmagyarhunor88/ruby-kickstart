e457c2f5ef40a1fee370f502c0c9656dbd9cfe94
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-22 18:49:52 +0000

    Sort of closer to solve challenge 7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,24 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  text = ""
+  r_exist = false
+  string.split('').each do |chr|
+    if r_exist == true
+      text << chr
+      r_exist = false
+    elsif chr.downcase == "r"
+      r_exist = true
+    end
+  end
+  text
 end
+
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?")
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")
