a3fb462b4db824cfdd0c6af1c6313269714c20dc
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-05-31 08:22:29 +0100

    Add sess2/chall8 details for refactor

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -6,9 +6,9 @@
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
 
-def got_three?(array)
-  container = [false]
-  array.each_index do |index|
+def got_three?(array) # need to refactor to deal with edge case
+  container = [false] # of nil, if last object is nil
+  array.each_index do |index| # will return true cos of this method
     if (array[index] == array[index + 1] && array[index] == array[index + 2])
       container[0] = true
     end
